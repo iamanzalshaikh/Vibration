90cfa4c204fb85d85a65ff872ff56e2c8fa76af9
iamanzalshaikh <tocxanzalftw49@gmail.com>
2025-09-27 21:30:12 +0530

    Update server.js

--- my-fullstack-app/backend/server.js
+++ my-fullstack-app/backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS only for frontend
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'https://vibration-q6co.onrender.com'
 }));
 
 app.use(express.json());
